7bd4e05bcad79798e451a0d7ace9a6421f5c9b7f
raphaelcott <52966113+raphaelcott@users.noreply.github.com>
2020-01-23 00:28:22 +0100

    Add files via upload

---
+++ Réservoir/reservoir.js
@@ -0,0 +1,21 @@
+$('.owl-nome').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: true,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>"
+      ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
